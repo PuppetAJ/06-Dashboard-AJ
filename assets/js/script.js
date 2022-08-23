a83b4dc32f2d7862667fb751a338a2be576a2bf2
Egg <adrianjimenez1950@gmail.com>
2022-08-23 05:00:42 -0400

    js functionality complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var cities = [];
 var currentCityName = "";
 var cityIconEl = document.querySelector("#currentIcon");
 var cityInputEl = document.querySelector("#formInput");
@@ -8,6 +9,10 @@ var cityWindEl = document.querySelector("#cityWind");
 var cityHumidityEl = document.querySelector("#cityHumidity");
 var cityUVIEl = document.querySelector("#cityUVI");
 var forecastContainerEl = document.querySelector("#forecastContainer");
+var dataContainerEl = document.querySelector("#dataContainer");
+var formContainerEl = document.querySelector("#formContainer");
+var buttonContainerEl = document.querySelector("#buttonContainer");
+var clearHistoryEl = document.querySelector("#deleteHistory");
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=8a42d43f7d7dc180da5b1e51890e67dc')
     .then(function (res) {
@@ -26,10 +31,124 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=8a42d43f7
 
     });
 
-// change api keys later
-// one call api documentation https://openweathermap.org/api/one-call-api
+// Change api keys later, currently using Jack's
+// One call api documentation https://openweathermap.org/api/one-call-api
+
+var citySearch = function(cityName) {
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&appid=8a42d43f7d7dc180da5b1e51890e67dc')
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            currentCityName = data.name;
+            cityInputEl.value = ""
+            return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly&units=imperial&appid=8a42d43f7d7dc180da5b1e51890e67dc`)
+        })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            applyCityStats(data.current);
+            forecastContainerEl.remove();
+            var newFCEl = document.createElement("div");
+            newFCEl.classList.add("d-flex");
+            newFCEl.setAttribute("id", "forecastContainer");
+            dataContainerEl.appendChild(newFCEl);
+            forecastConstructor(data.daily);
+
+            if (cities.length === 0) {
+
+                var cityObject = {
+                    name: currentCityName
+                }
+    
+                cities.unshift(cityObject);
+
+                saveCities();
+                buttonContainerConstructor();
+                buttonHistoryConstructor();
+
+            } else if (cities.length < 8) {
+
+                for (i = 0 ; i < cities.length; i++) {
+
+                   if (cities[i].name == currentCityName) {
+
+                        break;
+    
+                    } else if (i === cities.length - 1) {
+
+                        var cityObject = {
+                            name: currentCityName
+                        }
+            
+                        cities.unshift(cityObject);
+
+                        saveCities();
+                        buttonContainerConstructor();
+                        buttonHistoryConstructor();
+    
+                    }
+                }
+            
+            } else {
+
+                for (i = 0 ; i < cities.length; i++) {
+
+                    if (cities[i].name == currentCityName) {
+ 
+                         break;
+     
+                    } else if (i === cities.length - 1) {
+ 
+                        var cityObject = {
+                            name: currentCityName
+                        }
+
+                        cities.pop()
+                        cities.unshift(cityObject);
+
+                        saveCities();
+                        buttonContainerConstructor();
+                        buttonHistoryConstructor();
+     
+                    }
+
+                }
+            }
+
+        })
+        .catch(function () {
+            alert("Couldn't process request!");
+            currentCityName = "";
+            cityInputEl.value = "";
+
+        });
+
+};
+
+var buttonContainerConstructor = function() {
+    buttonContainerEl.remove()
+    var newBCEl = document.createElement("div");
+    newBCEl.setAttribute("id", "buttonContainer");
+    formContainerEl.appendChild(newBCEl);
+};
+
+var citySearchInput = function(event) {
+
+    event.preventDefault();
+    var csText = cityInputEl.value.trim();
+    if (csText === null || csText === "") {
+        alert("Input must not be empty!");
+    } else {
+        citySearch(csText);
+    }
+
+};
 
 var applyCityStats = function(currentStats) {
+
     cityTime = moment.unix(currentStats.dt).format('L');
     cityIconEl.setAttribute("src", "http://openweathermap.org/img/wn/"+currentStats.weather[0].icon+".png");
     cityNameEl.textContent = currentCityName + " (" + cityTime + ")";
@@ -51,14 +170,17 @@ var applyCityStats = function(currentStats) {
         cityUVIEl.classList.remove('bg-warning');
         cityUVIEl.classList.add('bg-danger');
     }
+
 };
 
 var forecastConstructor = function(dailyStats) {
+
+    forecastContainerEl = document.querySelector("#forecastContainer");
+
     for (n=1; n<6; n++) {
 
         var unixTime = dailyStats[n].dt;
         var formattedTime = moment.unix(unixTime).format('L');
-        console.log(formattedTime);
 
         var imgEl = document.createElement("img");
         var forecastCardEl = document.createElement("div");
@@ -66,6 +188,7 @@ var forecastConstructor = function(dailyStats) {
         var h2El = document.createElement("h2");
 
         forecastCardEl.classList.add("forecastCard");
+        forecastCardEl.classList.add("col");
         divEl.classList.add("p-3");
         h2El.setAttribute("id", "day-" + n);
         imgEl.setAttribute("src", "http://openweathermap.org/img/wn/"+dailyStats[n].weather[0].icon+".png")
@@ -94,27 +217,84 @@ var forecastConstructor = function(dailyStats) {
         forecastContainerEl.appendChild(forecastCardEl);
 
     }
-}
-
-var citySearchInput = function(event) {
-
-    event.preventDefault();
-    var csText = cityInputEl.value.trim();
-    if (csText === null || csText === "") {
-        alert("Invalid city");
-    } else {
-        console.log(csText);
-    }
-
 };
 
+var saveCities = function() {
+    localStorage.setItem("citiesT2b3XQw67Q6n", JSON.stringify(cities));
+};
+
+var loadCities = function() {
+
+    var loadedCities = JSON.parse(localStorage.getItem("citiesT2b3XQw67Q6n"));
+
+    if (!loadedCities) {
+        return false;
+    } else {
+        cities = loadedCities;
+        buttonHistoryConstructor();
+        console.log(cities);
+    }
+};
+
+var buttonHistoryEvent = function(event) {
+
+    var target = event.target;
+    var targetText = target.textContent
+    citySearch(targetText);
+
+}
+
+var buttonHistoryConstructor = function() {
+
+    buttonContainerEl = document.querySelector("#buttonContainer");
+
+    if (cities.length > 0 && cities.length < 8) {
+        for (i=0; i < cities.length; i++) {
+            var buttonEl = document.createElement("button");
+            buttonEl.classList.add("btn");
+            buttonEl.classList.add("btnFormat");
+            buttonEl.classList.add("btn-secondary");
+            buttonEl.classList.add("text-dark");
+            buttonEl.textContent = cities[i].name;
+            buttonEl.addEventListener("click", buttonHistoryEvent);
+
+            buttonContainerEl.appendChild(buttonEl);
+        }
+    } else if (cities.length >= 8) {
+        for (i=0; i < 8; i++) {
+            var buttonEl = document.createElement("button");
+            buttonEl.classList.add("btn");
+            buttonEl.classList.add("btnFormat");
+            buttonEl.classList.add("btn-secondary");
+            buttonEl.classList.add("text-dark");
+            buttonEl.textContent = cities[i].name;
+            buttonEl.addEventListener("click", buttonHistoryEvent);
+
+            buttonContainerEl.appendChild(buttonEl);
+        }
+    } else {
+        return;
+    }
+
+}
+
+var clearHistory = function() {
+    cities = [];
+    saveCities();
+    loadCities();
+    buttonContainerConstructor();
+}
+
+
+loadCities();
 citySubmitEl.addEventListener("click", citySearchInput);
+clearHistoryEl.addEventListener("click", clearHistory);
+
 
 /* 
     To DO:
-    - Use input from form button to call a fetch function
     - change API keys
     - style the page better
-    - add recent searches to local storage
-    - add clear button to said local storage stuff
+    - make page responsive
+    - https://stackoverflow.com/questions/31944691/bootstrap-5-column-layout
 */ 
